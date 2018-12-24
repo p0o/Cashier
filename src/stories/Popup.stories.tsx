81b1c05b249af9043ab61531fbf5e62ecd5d0b2f
p0o <p0oker@protonmail.com>
2018-12-24 19:23:35 +0800

    make storybook to work with typescript

---
+++ src/stories/Popup.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import App from '../App';
+console.log('hya');
+
+storiesOf('Welcome', module).add('to Storybook', () => <App/>);
+
