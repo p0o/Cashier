cbac286a7065c8ca3f668da863fc51cc5e1a68e6
p0o <p0oker@protonmail.com>
2018-12-25 23:18:41 +0800

    add wallet creation steps

--- src/createWallet/CreateWallet.tsx
+++ src/createWallet/CreateWallet.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Steps, Icon } from 'antd';
 import { Link } from 'route-lite';
 
-type Props = {name?: string};
+import 'antd/lib/steps/style/css'
+import 'antd/lib/icon/style/css';
+import 'antd/lib/col/style/css';
+import 'antd/lib/row/style/css';
+import 'antd/lib/button/style/css';
+
+type Props = any;
+const { Step } = Steps;
 
 function CreateWallet(props: Props) {
   return (
     <Row>
       <Col span={20} offset={2}>
+        <Row style={{marginTop: 15, marginBottom: 15}}>
+          <Steps size="small">
+            <Step status="process" title="Start" />
+            <Step status="wait" title="Seed"  />
+            <Step status="wait" title="Confirm" />
+            <Step status="wait" title="Done" icon={<Icon type="smile-o" />} />
+          </Steps>
+        </Row>
         <Row>
           <p>You don't have a wallet yet, click below to create a local Bitcoin Cash wallet.</p>
         </Row>
         <Row>
-          <Button>Create New Wallet</Button>
+          <Button type="primary">Create New Wallet</Button>
         </Row>
       </Col>
     </Row>
