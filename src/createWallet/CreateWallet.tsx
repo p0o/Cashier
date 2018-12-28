740a9ea9a99f0cced085a984b659bab61d887271
p0o <p0oker@protonmail.com>
2018-12-28 12:52:05 +0800

    implement step changes for wallet creation

--- src/createWallet/CreateWallet.tsx
+++ src/createWallet/CreateWallet.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button, Steps, Icon } from 'antd';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import { Link } from 'route-lite';
 
 import 'antd/lib/steps/style/css'
@@ -7,28 +8,64 @@ import 'antd/lib/icon/style/css';
 import 'antd/lib/col/style/css';
 import 'antd/lib/row/style/css';
 import 'antd/lib/button/style/css';
+import './CreateWallet.css';
 
 type Props = any;
 const { Step } = Steps;
 
 function CreateWallet(props: Props) {
+  const [slideNo, setSlideNo] = useState(0);
+
+  function stepStatus(current: number): 'finish' | 'wait' | 'process' {
+    if (slideNo > current) return 'finish';
+    if (slideNo < current) return 'wait';
+    return 'process';
+  }  
+
   return (
     <Row>
-      <Col span={20} offset={2}>
+      <Col span={20} offset={2} style={{overflow: 'hidden'}}>
         <Row style={{marginTop: 15, marginBottom: 15}}>
           <Steps size="small">
-            <Step status="process" title="Start" />
-            <Step status="wait" title="Seed"  />
-            <Step status="wait" title="Confirm" />
-            <Step status="wait" title="Done" icon={<Icon type="smile-o" />} />
+            <Step
+              status={stepStatus(0)}
+              title="Start"
+              onClick={() => setSlideNo(0)}
+            />
+            <Step
+              status={stepStatus(1)}
+              title="Seed"
+              onClick={() => setSlideNo(1)}
+            />
+            <Step
+              status={stepStatus(2)}
+              title="Confirm"
+              onClick={() => setSlideNo(2)}
+            />
+            <Step
+              status={stepStatus(3)}
+              title="Done"
+              icon={<Icon type="smile-o"/>}
+              onClick={() => setSlideNo(3)}
+            />
           </Steps>
         </Row>
-        <Row>
-          <p>You don't have a wallet yet, click below to create a local Bitcoin Cash wallet.</p>
-        </Row>
-        <Row>
-          <Button type="primary">Create New Wallet</Button>
-        </Row>
+        <Flipper flipKey={slideNo}>
+          <div className="slidingBox" style={{left: -458 * slideNo}}>
+            <Flipped flipId="1">
+              <div className="slidingBoxItem">
+                <Row>
+                  <p>You don't have a wallet yet, click below to create an encrypted Bitcoin Cash wallet inside your browser.</p>
+                </Row>
+                <Row>
+                  <Button type="primary" onClick={() => setSlideNo(slideNo + 1)}>Create New Wallet</Button>
+                </Row>
+              </div>
+            </Flipped>
+            
+          </div>
+        </Flipper>
+          
       </Col>
     </Row>
   )
