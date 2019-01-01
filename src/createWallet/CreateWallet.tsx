16622532ae50e5a663e99512b3afc05ca49ff19a
p0o <p0oker@protonmail.com>
2019-01-01 22:08:43 +0800

    map steps to reduce boilerplate

--- src/createWallet/CreateWallet.tsx
+++ src/createWallet/CreateWallet.tsx
@@ -9,9 +9,11 @@ import 'antd/lib/col/style/css';
 import 'antd/lib/row/style/css';
 import 'antd/lib/button/style/css';
 import './CreateWallet.css';
+import CreateWalletStart from './CreateWalletStart';
 
 type Props = any;
 const { Step } = Steps;
+const steptTitleList = ['Start', 'Seed', 'Confirm', 'Done'];
 
 function CreateWallet(props: Props) {
   const [slideNo, setSlideNo] = useState(0);
@@ -27,39 +29,24 @@ function CreateWallet(props: Props) {
       <Col span={20} offset={2} style={{overflow: 'hidden'}}>
         <Row style={{marginTop: 15, marginBottom: 15}}>
           <Steps size="small">
-            <Step
-              status={stepStatus(0)}
-              title="Start"
-              onClick={() => setSlideNo(0)}
-            />
-            <Step
-              status={stepStatus(1)}
-              title="Seed"
-              onClick={() => setSlideNo(1)}
-            />
-            <Step
-              status={stepStatus(2)}
-              title="Confirm"
-              onClick={() => setSlideNo(2)}
-            />
-            <Step
-              status={stepStatus(3)}
-              title="Done"
-              icon={<Icon type="smile-o"/>}
-              onClick={() => setSlideNo(3)}
-            />
+            {
+              steptTitleList.map((title, stepIndex) => (
+                <Step
+                  status={stepStatus(stepIndex)}
+                  title={title}
+                  onClick={() => setSlideNo(stepIndex)}
+                />
+              ))
+            }
           </Steps>
         </Row>
         <Flipper flipKey={slideNo}>
           <div className="slidingBox" style={{left: -458 * slideNo}}>
             <Flipped flipId="1">
               <div className="slidingBoxItem">
-                <Row>
-                  <p>You don't have a wallet yet, click below to create an encrypted Bitcoin Cash wallet inside your browser.</p>
-                </Row>
-                <Row>
-                  <Button type="primary" onClick={() => setSlideNo(slideNo + 1)}>Create New Wallet</Button>
-                </Row>
+                <CreateWalletStart
+                  goToNextStep={() => setSlideNo(slideNo + 1)}
+                />
               </div>
             </Flipped>
             
