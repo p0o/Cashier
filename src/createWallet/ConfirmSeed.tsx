f1049f1d43e5c41af1dcae70371ffd43467757cc
p0o <p0oker@protonmail.com>
2019-01-10 02:11:46 +0800

    adjust UI after seed is matched

--- src/createWallet/ConfirmSeed.tsx
+++ src/createWallet/ConfirmSeed.tsx
@@ -24,22 +24,32 @@ function ConfirmSeed({ goToNextStep, seed }: Props) {
     setEnteredSeed(seedValues);
   }
 
+  const seedIsMatched = seedMatches(enteredSeed, seedList);
+
   return (
     <React.Fragment>
-      <Row>Please type-in your seed below so we can confirm you stored it safely. The first 7 words are enough to confirm.</Row>
+      { !seedIsMatched ?
+        <Row>Please type-in your seed below so we can confirm you stored it safely. The first 7 words are enough to confirm.</Row>
+      :
+        <Row>Seems like you really wrote down your seed! Click on the button below to get into your wallet soon!</Row>
+      }
+      
       <Row style={{marginTop: 16}}>
         <Select
           style={{width: '100%'}}
           mode="multiple"
           placeholder="Choose your recovery seed words, one by one"
           onChange={handleSeedConfirmation}
+          disabled={seedIsMatched}
+          open={!seedIsMatched}
         >
           { seedSelectChildren }
         </Select>
       </Row>
-      <Row>
-        { seedMatches(enteredSeed, seedList) &&
-          <Button onClick={goToNextStep}>Done</Button>
+     
+      <Row style={{marginTop: 16}}>
+        { seedIsMatched &&
+          <Button type="primary" onClick={goToNextStep}>Open Wallet</Button>
         }
       </Row>
     </React.Fragment>
