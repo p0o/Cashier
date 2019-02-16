0764aca786b14c3f943b52c6df1084cc26fc2193
p0o <p0oker@protonmail.com>
2019-02-16 15:48:02 +0800

    generate mnemonic for the wallet

--- src/createWallet/CreateWallet.tsx
+++ src/createWallet/CreateWallet.tsx
@@ -1,56 +1,53 @@
-import React, { useState } from 'react';
-import { Row, Col, Button, Steps, Icon } from 'antd';
-import { Flipper, Flipped } from 'react-flip-toolkit';
-import { Link } from 'route-lite';
+import React, { useState } from "react";
+import { Row, Col, Button, Steps, Icon } from "antd";
+import { Flipper, Flipped } from "react-flip-toolkit";
+import { Link } from "route-lite";
 
-
-import './CreateWallet.css';
-import CreateWalletStart from './CreateWalletStart';
-import GenerateSeed from './GenerateSeed';
-import ConfirmSeed from './ConfirmSeed';
-import Done from './Done';
+import "./CreateWallet.css";
+import CreateWalletStart from "./CreateWalletStart";
+import GenerateSeed from "./GenerateSeed";
+import ConfirmSeed from "./ConfirmSeed";
+import Done from "./Done";
 
 type Props = any;
 const { Step } = Steps;
-const steptTitleList = ['Start', 'Seed', 'Confirm', 'Done'];
+const steptTitleList = ["Start", "Seed", "Confirm", "Done"];
 
 function CreateWallet(props: Props) {
   const [slideNo, setSlideNo] = useState(0);
-  const [currentSeed, setCurrentSeed] = useState('');
+  const [currentSeed, setCurrentSeed] = useState("");
 
-  function stepStatus(current: number): 'finish' | 'wait' | 'process' {
-    if (slideNo > current) return 'finish';
-    if (slideNo < current) return 'wait';
-    return 'process';
+  function stepStatus(current: number): "finish" | "wait" | "process" {
+    if (slideNo > current) return "finish";
+    if (slideNo < current) return "wait";
+    return "process";
   }
 
   function handleGeneratedSeed(seed: string): void {
     setCurrentSeed(seed);
   }
 
-  function handleJumpToAnotherStep(targetStepNum : number) {
+  function handleJumpToAnotherStep(targetStepNum: number) {
     if (targetStepNum >= slideNo) return;
     setSlideNo(targetStepNum);
   }
 
   return (
     <Row>
-      <Col span={20} offset={2} style={{overflow: 'hidden'}}>
-        <Row style={{marginTop: 15, marginBottom: 15}}>
+      <Col span={20} offset={2} style={{ overflow: "hidden" }}>
+        <Row style={{ marginTop: 15, marginBottom: 15 }}>
           <Steps size="small">
-            {
-              steptTitleList.map((title, stepIndex) => (
-                <Step
-                  status={stepStatus(stepIndex)}
-                  title={title}
-                  onClick={() => handleJumpToAnotherStep(stepIndex)}
-                />
-              ))
-            }
+            {steptTitleList.map((title, stepIndex) => (
+              <Step
+                status={stepStatus(stepIndex)}
+                title={title}
+                onClick={() => handleJumpToAnotherStep(stepIndex)}
+              />
+            ))}
           </Steps>
         </Row>
         <Flipper flipKey={slideNo}>
-          <div className="slidingBox" style={{left: -458 * slideNo}}>
+          <div className="slidingBox" style={{ left: -458 * slideNo }}>
             <Flipped flipId="1" key="1">
               <div className="slidingBoxItem">
                 <CreateWalletStart
@@ -61,40 +58,34 @@ function CreateWallet(props: Props) {
 
             <Flipped flipId="2" key="2">
               <div className="slidingBoxItem">
-                {slideNo === 1 &&
+                {slideNo === 1 && (
                   <GenerateSeed
                     goToNextStep={() => setSlideNo(slideNo + 1)}
                     onGenerateSeed={handleGeneratedSeed}
                   />
-                }
+                )}
               </div>
             </Flipped>
 
             <Flipped flipId="3" key="3">
               <div className="slidingBoxItem">
-                {slideNo === 2 &&
+                {slideNo === 2 && (
                   <ConfirmSeed
                     goToNextStep={() => setSlideNo(slideNo + 1)}
                     seed={currentSeed}
                   />
-                }
+                )}
               </div>
             </Flipped>
 
-            <Flipped flipId="4">
-              <div className="slidingBoxItem">
-                {slideNo === 3 &&
-                  <Done />
-                }
-              </div>
+            <Flipped flipId="4" key="4">
+              <div className="slidingBoxItem">{slideNo === 3 && <Done />}</div>
             </Flipped>
-            
           </div>
         </Flipper>
-          
       </Col>
     </Row>
-  )
+  );
 }
 
 export default CreateWallet;
--- src/createWallet/GenerateSeed.tsx
+++ src/createWallet/GenerateSeed.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Popover, Progress, Button } from 'antd';
+import BitboxSDK from 'bitbox-sdk/lib/bitbox-sdk';
+
+const Bitbox = new BitboxSDK();
 
 import 'antd/lib/popover/style/css';
 import 'antd/lib/progress/style/css';
 import 'antd/lib/button/style/css';
 
 type Props = {
-  goToNextStep: () => void,
-  onGenerateSeed: (seed: string) => void,
+  goToNextStep: () => void;
+  onGenerateSeed: (seed: string) => void;
 };
-
-const sampleMnemonic = 'disagree tide elbow citizen jazz cinnamon bridge certain april settle pact film always inmate border inform solution that submit produce cloth balcony upper maid';
 
 function GenerateSeed({ goToNextStep, onGenerateSeed }: Props) {
   const [fakeProgress, setFakeProgress] = useState(0);
@@ -18,53 +19,78 @@ function GenerateSeed({ goToNextStep, onGenerateSeed }: Props) {
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
+    if (!seed) {
+      // 12 words mnemonic
+      const mnemonic = Bitbox.Mnemonic.generate(128);
+      setSeed(mnemonic);
+    }
+
     if (fakeProgress >= 100) {
-      setSeed(sampleMnemonic);
-      onGenerateSeed(sampleMnemonic);
+      onGenerateSeed(seed);
       return;
     }
-    
+
     // don't take it seriously, just giving a false sense of progress for UX reasons
-    const increment =  fakeProgress < 80 ? 40 : fakeProgress >= 80 ? 18 : 10; 
+    const increment = fakeProgress < 80 ? 40 : fakeProgress >= 80 ? 18 : 10;
     setTimeout(() => setFakeProgress(fakeProgress + increment), 300);
   }, [fakeProgress]);
 
   return (
     <div>
-      { fakeProgress < 100 &&
+      {fakeProgress < 100 && (
         <Row>
           Generating Recovery Seed
           <Progress percent={fakeProgress} />
         </Row>
-      }
+      )}
 
-      { fakeProgress >= 100 &&
+      {fakeProgress >= 100 && (
         <div>
-          <Row style={{textAlign: 'left'}}>
-            A very strong recovery seed has been generated for you!
-            {' '}
+          <Row style={{ textAlign: 'left' }}>
+            A very strong recovery seed has been generated for you!{' '}
             <Popover
               title="What is a Recovery Seed"
-              content={<p style={{maxWidth: '400px'}}>A recover seed is a series of phrases that can be used to recover all your wallets.</p>}
+              content={
+                <p style={{ maxWidth: '400px' }}>
+                  A recover seed is a series of phrases that can be used to
+                  recover all your wallets.
+                </p>
+              }
               visible={showGuide}
               onVisibleChange={() => setShowGuide(!showGuide)}
             >
-              <span style={{color: '#595959', fontSize: '12px', fontWeight: 'bold'}}>
+              <span
+                style={{
+                  color: '#595959',
+                  fontSize: '12px',
+                  fontWeight: 'bold',
+                }}
+              >
                 (What is this?)
               </span>
             </Popover>
-
             <br />
-            <p style={{fontWeight: 'bold', textAlign: 'center'}}>Write it down on paper and keep it secretly.</p>
-            <div style={{textAlign: 'center', background: '#d9d9d9', borderRadius: '5px', padding: '5px'}}>
-              { seed }
+            <p style={{ fontWeight: 'bold', textAlign: 'center' }}>
+              Write it down on paper and keep it secretly.
+            </p>
+            <div
+              style={{
+                textAlign: 'center',
+                background: '#d9d9d9',
+                borderRadius: '5px',
+                padding: '5px',
+              }}
+            >
+              {seed}
             </div>
           </Row>
-          <Row style={{marginTop: 10}}>
-            <Button type="dashed" onClick={goToNextStep}>I did safely write it down</Button>
+          <Row style={{ marginTop: 10 }}>
+            <Button type="dashed" onClick={goToNextStep}>
+              I did safely write it down
+            </Button>
           </Row>
         </div>
-      }
+      )}
     </div>
   );
 }
